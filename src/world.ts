142f99ad59b3bd2fd9903b2dd8bbcfa817c9b974
gpi <gpimpale29@gmail.com>
2022-05-17 18:12:03 -0700

    world

--- src/world.ts
+++ src/world.ts
@@ -1,65 +1,95 @@
-import {makeNoise3D} from 'open-simplex-noise';
+import { makeNoise3D } from 'open-simplex-noise';
 
-import {vec3} from './utils';
+import { vec3 } from './utils';
 
 const CHUNK_X_SIZE = 16;
 const CHUNK_Y_SIZE = 16;
 const CHUNK_Z_SIZE = 16;
 
+type Chunk = {
+  // array must be CHUNK_X_SIZE*CHUNK_Y_SIZE*CHUNK_Z_SIZE
+  blocks: Uint16Array,
+  buffer: WebGLBuffer
+}
+
 class World {
+
   private centerLoc: vec3;
 
   // noise function
   private readonly seed: number;
-  private readonly noiseFn: (x:number, y:number, z:number) => number;
+  private readonly noiseFn: (x: number, y: number, z: number) => number;
 
   // hashmap storing chunks
-  private chunk_map: Map<string, { blocks: Uint16Array, buffer: WebGLBuffer}>;
+  private chunk_map: Map<string, Chunk>;
 
   // vector of the coordinates of chunks to generate
-  ivec3_vec *togenerate;
-  // vector of the coordinates of chunks that are asynchronously generating
-  ivec3_vec *generating;
+  private togenerate: vec3[];
   // vector of the coordinates of chunks to mesh
-  ivec3_vec *tomesh;
+  private tomesh: vec3[];
   // vector of the coordinates of ready chunks
-  ivec3_vec *ready;
+  private ready: vec3[];
   // vector of the coordinates of chunks to unload
-  ivec3_vec *tounload;
+  private tounload: vec3[];
+
+  constructor(seed: number, centerLoc:vec3) {
+    this.seed = seed;
+    this.noiseFn = makeNoise3D(seed);
+    this.centerLoc = centerLoc;
+    this.chunk_map = new Map();
+
+    this.togenerate = [];
+    this.tomesh = [];
+    this.ready = [];
+    this.tounload = [];
+  }
+
+
 
 
 }
 
 // generate chunk data
-void worldgen_state_gen_chunk(    //
-    ChunkData *pCd,               //
-    const ivec3 worldChunkCoords, //
-    const worldgen_state *state   //
-) {
+function genChunkData(    //
+  worldChunkCoords: vec3, //
+  noise: (x: number, y: number, z: number) => number//
+): Uint16Array {
   // generate chunk, we need to give it the block coordinate to generate at
-  vec3 chunkOffset;
-  worldChunkCoords_to_blockCoords(chunkOffset, worldChunkCoords);
+  const chunkOffset = [
+      worldChunkCoords[0] * CHUNK_X_SIZE,
+      worldChunkCoords[1] * CHUNK_Y_SIZE,
+      worldChunkCoords[2] * CHUNK_Z_SIZE
+  ];
 
-  double scale1 = 20.0;
-  for (uint32_t x = 0; x < CHUNK_X_SIZE; x++) {
-    for (uint32_t y = 0; y < CHUNK_Y_SIZE; y++) {
-      for (uint32_t z = 0; z < CHUNK_Z_SIZE; z++) {
+  const blocks = new Uint16Array(CHUNK_X_SIZE * CHUNK_Y_SIZE * CHUNK_Z_SIZE);
+
+
+  const scale1 = 20.0;
+  for (let x = 0; x < CHUNK_X_SIZE; x++) {
+    const off_x = x * CHUNK_Y_SIZE * CHUNK_Z_SIZE;
+    for (let y = 0; y < CHUNK_Y_SIZE; y++) {
+      const off_xy = off_x + y * CHUNK_Y_SIZE;
+      for (let z = 0; z < CHUNK_Z_SIZE; z++) {
+        // this is the offset within the blocks array to store the value
+        const off_xyz = off_xy + z;
+
         // calculate world coordinates in blocks
-        double wx = x + (double)chunkOffset[0];
-        double wy = y + (double)chunkOffset[1];
-        double wz = z + (double)chunkOffset[2];
-        double val = open_simplex_noise3(state->noiseCtx, wx / scale1,
-                                         wy / scale1, wz / scale1);
-        double val2 = open_simplex_noise3(state->noiseCtx, wx / scale1,
-                                          (wy - 1) / scale1, wz / scale1);
+        const wx = x + chunkOffset[0];
+        const wy = y + chunkOffset[1];
+        const wz = z + chunkOffset[2];
+        const val = noise(wx / scale1, wy / scale1, wz / scale1);
+        const val2 = noise(wx / scale1, (wy - 1) / scale1, wz / scale1);
+
         if (val > 0 && val2 < 0) {
-          pCd->blocks[x][y][z] = 1; // grass
+          blocks[off_xyz] = 1; // grass
         } else if (val > 0) {
-          pCd->blocks[x][y][z] = 2; // grass
+          blocks[off_xyz] = 2; // grass
         } else {
-          pCd->blocks[x][y][z] = 0; // air
+          blocks[off_xyz] = 0; // air
         }
       }
     }
   }
+
+  return blocks;
 }
